5bf55b600a88b4025f9a0744dee4b1a9c9443965
Martin Rünk <martin.rynk@gmail.com>
2025-02-24 14:11:34 +0200

    Reformat code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AirportsComponent } from './components/airports/airports.component';
-import { FlightsComponent } from './components/flights/flights.component';
-import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'flight-info';
-}
+export class AppComponent {}
--- src/app/components/airports/airports.component.ts
+++ src/app/components/airports/airports.component.ts
@@ -1,20 +1,39 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { AirportService } from '../../services/airport.service';
 import { Airport } from '../../model/airport.type';
-import { FormsModule } from '@angular/forms';
-import { catchError, map, tap, throwError } from 'rxjs';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { catchError, map, Observable, startWith, tap, throwError } from 'rxjs';
 import { FilterAirportsPipe } from '../../pipes/filter-airports.pipe';
 import { environment } from '../../../environments/environment';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-airports',
-  imports: [FormsModule, FilterAirportsPipe],
+  imports: [
+    FormsModule,
+    FilterAirportsPipe,
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    AsyncPipe,
+    ReactiveFormsModule,
+  ],
   templateUrl: './airports.component.html',
   styleUrl: './airports.component.css',
 })
 export class AirportsComponent implements OnInit {
   airportService = inject(AirportService);
   searchTerm = signal('');
+
+  airportOptions = this.airportService.airports();
+
+  myControl = new FormControl<string | Airport>('');
+  filteredOptions = new Observable<Airport[]>();
 
   ngOnInit(): void {
     if (environment.isRequestLive) {
@@ -50,6 +69,27 @@ export class AirportsComponent implements OnInit {
         foundAirport || airports[0] || null
       );
     }
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => {
+        const name = typeof value === 'string' ? value : value?.name;
+        return name
+          ? this._filter(name as string)
+          : this.airportService.airports();
+      })
+    );
+  }
+
+  private _filter(value: string): Airport[] {
+    const filterValue = value.toLowerCase();
+
+    return this.airportService
+      .airports()
+      .filter((option) => option.name.toLowerCase().includes(filterValue));
+  }
+
+  displayFn(airport: Airport): string {
+    return airport && airport.name ? airport.name : '';
   }
 
   selectAirport(airport: any) {
